5bcfa712e6c4ea46dfcebe1fffcef84ee25e072d
Alquimista-07 <jdca.07@gmail.com>
2022-04-12 12:53:42 -0500

    9. Los tipos de datos numéricos binarios y octales

---
+++ Sección 2_Mejoras a las variables y tipos de datos/4. JavaScript/js/app.js
@@ -0,0 +1,14 @@
+// 9. Los tipos de datos numéricos binarios y octales
+
+//Literales octales y binarios
+//
+// Hay ocasiones en que el contexto de nuestros datos requere que
+// trabajemos con cifras no decimales, por ejemplo en base 2 (binario) o base
+// 8 (octal), ahora es sencillo crear este tipo de literales con los prefijos (0b) y
+// (0o) respectivamente.
+
+var b = 0b111110111; // Binario
+console.log( b );     // 503
+
+var o = 0o767;    // Octal
+console.log( o ); // 503
